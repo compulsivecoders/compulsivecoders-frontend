7909da54649c3a78b8bc0195f91cb66338aff4d4
Gilbert Tchoi <g17569832+gilberttchoi@users.noreply.github.com>
2020-04-08 23:09:46 +0200

    fix php lang

--- plugins/prism.js
+++ plugins/prism.js
@@ -1,4 +1,9 @@
 import Prism from 'prismjs'
+import loadLanguages from 'prismjs/components/index'
 import 'prismjs/themes/prism.css'
+import 'prismjs/components/prism-markup-templating'
+import 'prismjs/components/prism-php'
+
+loadLanguages(['markup-templating', 'php'])
 
 export default Prism
